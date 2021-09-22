930c62be66983a1ed2d3e2076607924fdb677051
tamitakada <tamitakada@gmail.com>
2021-09-22 18:03:15 -0400

    more optimized for diff numbers of periods

--- softdev_name_print.py
+++ softdev_name_print.py
@@ -1,37 +1,23 @@
 def printNames(period, index):
-    pd1 = []
-    pd2 = []
-
-    with open('softdev_students_pd1.txt') as reader:
-        name = reader.readline()
-        while name != '':
-            pd1.append(name.rstrip('\n'))
+    pds = []
+    
+    for i in range(2):
+        pd = []
+        fileName = 'softdev_students_pd' + str(i + 1) + '.txt'
+        
+        with open(fileName) as reader:
             name = reader.readline()
-
-    with open('softdev_students_pd2.txt') as reader:
-        name = reader.readline()
-        while name != '':
-            pd2.append(name.rstrip('\n'))
-            name = reader.readline()
-        pd1.sort()
-        pd2.sort()
-
-    if period == 1:
-        if index < len(pd1):
-            print(pd1[index])
-        else:
-            print('Student does not exist')
-    elif period == 2:
-        if index < len(pd2):
-            print(pd2[index])
+            while name != '':
+                pd.append(name.rstrip('\n'))
+                name = reader.readline()
+        
+        pd.sort()
+        pds.append(pd)
+        
+    if period > 0 and period < 3:
+        if index < len(pds[period - 1]):
+            print(pds[period - 1][index])
         else:
             print('Student does not exist')
     else:
         print('Period does not exist')
-    
-   
-    print(pd1)
-    print(pd2)
-
-printNames(1, 2)
-printNames(2, 7)
