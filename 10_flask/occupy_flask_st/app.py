7a1148d67ac05e878993ab34bc5a11171c68b01d
Tami Takada <ttakada20@stuy.edu>
2021-10-05 08:24:20 -0400

    styles

--- 10_flask/occupy_flask_st/app.py
+++ 10_flask/occupy_flask_st/app.py
@@ -5,23 +5,50 @@
 
 from flask import Flask
 
+# Import methods from occupations.py
 import occupations
 
-app = Flask(__name__) #create instance of class Flask
+# Create instance of the Flask class
+app = Flask(__name__)
 
-@app.route("/")       #assign fxn to route
+
+@app.route("/")  # Assign function to route
 def display_occupation():
-    output = ""
+    # Print out trio name and roster and skips two lines
+    output = "STD Trio: Christopher Liu, Tina Nguyen, Tami Takada</br></br>"
 
+    # Creates dictionary called jobs which is a dictionary returned
+    # from read_occupations() which reads through occupations.csv and makes the
+    # keys job occupations and the values the percentages associated with it
     jobs = occupations.read_occupations("occupations.csv")
-    output = "<strong>Selected Job:</strong> " + occupations.choose_from_dict(jobs) + "</br></br>"
+
+    output += "<strong>Selected Job:</strong> "
+    # The randomly chosen job will be printed after this string
+    selected_job = occupations.choose_from_dict(jobs)
+    output += selected_job + "</br></br>"
+    # This prints out a randomly chosen occupation (weighted by the percents)
+    # that is returned from choose_from_dict() and skips two lines
 
     output += "<strong>List of Jobs:</strong></br>"
+    output += "<ul>"
+    
+    colors = ["red", "orange", "gold", "limegreen", "cyan", "magenta"]
+    num = 0;
+    
+    # Precedes a printed list of all the jobs in occupations.csv
     for job in jobs.keys():
-        output += job + "</br>"
+        # Loops through all the keys in the dictionary and prints the job out,
+        # going to the next line each time it prints
+        if job == selected_job:
+            output += "<li style='color: black; background-color: yellow'><strong>" + job + "</strong></li>"
+        else:
+            output += "<li style='color: " + colors[num % len(colors)] + "'>" + job + "</li>"
+            num += 1
+    output += "</ul>"
 
     return output
 
+
 if __name__ == "__main__":  # true if this file NOT imported
-    app.debug = True        # enable auto-reload upon code change
+    app.debug = True  # enable auto-reload upon code change
     app.run()
