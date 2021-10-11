5d01b803b0cd90be54d2e87eaee71594c4227848
tamitakada <tamitakada@gmail.com>
2021-10-10 23:06:32 -0400

    fixed organization

--- 13_tempwork/app.py
+++ 13_tempwork/app.py
@@ -4,19 +4,47 @@
 # 2021-10-10
 
 from flask import Flask, render_template
-import occupations
+import csv
+import random
+
+
+def read_occupations(filename: str) -> dict:
+    occupations = {}
+    with open(filename, newline="") as csvfile:
+        reader = csv.reader(csvfile)
+
+        next(reader)
+
+        for index, row in enumerate(reader):
+            job_class = row[0]
+            percentage = row[1]
+            occupations[job_class] = float(percentage)
+
+    total_percentage = occupations["Total"]
+    occupations["Other"] = 100 - total_percentage
+    del occupations["Total"]
+
+    return occupations
+
+
+def choose_from_dict(occupations: dict) -> str:
+    job_classes = list(occupations.keys())
+    percentages = list(occupations.values())
+
+    choice = random.choices(job_classes, weights=percentages)[0]
+    return choice
 
 app = Flask(__name__)
 
 @app.route("/occupyflaskst")
 def get_occupations_page():
-    collection = occupations.read_occupations("occupations.csv")
+    collection = read_occupations("data/occupations.csv")
     
     return render_template(
         "occupyflaskst.html",
         header = "Random Occupation",
         collection = collection,
-        randomoc = occupations.choose_from_dict(collection)
+        randomoc = choose_from_dict(collection)
     )
 
 if __name__ == "__main__":
