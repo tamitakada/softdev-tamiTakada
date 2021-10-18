f41fdf0e5aefc89dabc1a6e8d38e928251b01b20
Tami Takada <ttakada20@stuy.edu>
2021-10-18 08:39:39 -0400

    stuff

--- 14_form/app.py
+++ 14_form/app.py
@@ -3,12 +3,16 @@
 # K14 - Form and Function
 # 2021-10-14
 
+# Questions: what does the POST method do?
+
 from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 @app.route("/", methods=['GET', 'POST'])
+
 def disp_loginpage():
+    """Returns the login page using the login.html template (http://127.0.0.1:5000/)"""
     return render_template( 'login.html' )
 
 
@@ -16,8 +20,8 @@ def disp_loginpage():
 def authenticate():
     return render_template(
         'response.html',
-        username = request.args['username'],
-        method = request.method
+        username = request.args['username'], #request.args returns the value for the username key (that is, the username).
+        method = request.method #Uses the GET method to display the form.
     ) #response to a form submission
 
 
