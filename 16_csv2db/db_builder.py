38b312c9e7a93035277db669521fab486b9ec145
tamitakada <tamitakada@gmail.com>
2021-10-22 15:06:24 -0400

    addd heading and comments

--- 16_csv2db/db_builder.py
+++ 16_csv2db/db_builder.py
@@ -1,7 +1,7 @@
-#Clyde "Thluffy" Sinclair
+#CrocCatGin - Rayat Roy, Michelle Lo, Tami Takada
 #SoftDev
-#skeleton/stub :: SQLITE3 BASICS
-#Dec 2020 -- The Time of the Rona
+#K16 - All About Database
+#2021-10-20
 
 import sqlite3   #enable control of an sqlite database
 import csv       #facilitate CSV I/O
@@ -12,31 +12,34 @@ DB_FILE='discobandit.db'
 db = sqlite3.connect(DB_FILE) #open if file exists, otherwise create
 c = db.cursor()               #facilitate db ops -- you will use cursor to trigger db events
 
+#==========================================================
+
 def csv_to_db(filename, table, headers):
+    """ Reads from the csv file and inputs data into the specified table in the database.
+        The headers define the different columns in the csv file, and the first column
+        is assumed to be a string type. """
     with open(filename, newline='') as csvfile: # reading courses.csv
         reader = csv.DictReader(csvfile)
 
         for row in reader:
             command = 'INSERT INTO ' + table + ' VALUES (\"'
             for i in range(len(headers)):
-                command += row[headers[i]]
+                command += row[headers[i]] # adds values of each column
                 if i == len(headers) - 1:
                     command += ');'
-                elif i == 0:
+                elif i == 0: # closes quotation mark for string
                     command += '\", '
                 else:
                     command += ', '
             # print(command)
             c.execute(command)
 
-#==========================================================
-
-command = 'CREATE TABLE courses (name course, mark INTEGER KEY, id INTEGER KEY)'          # test SQL stmt in sqlite3 shell, save as string
+command = 'CREATE TABLE courses (name course, mark INTEGER KEY, id INTEGER KEY)' # adds courses table
 c.execute(command)    # run SQL statement
 
 csv_to_db('courses.csv', 'courses', ['code', 'mark', 'id'])
 
-command = 'CREATE TABLE students (name student, age INTEGER KEY, id INTEGER KEY)'          # test SQL stmt in sqlite3 shell, save as string
+command = 'CREATE TABLE students (name student, age INTEGER KEY, id INTEGER KEY)' # adds students table
 c.execute(command)
 
 csv_to_db('students.csv', 'students', ['name', 'age', 'id'])
