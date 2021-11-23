e87f4bc4b46224db8bc39862ab41106eb0302a0a
tamitakada <tamitakada@gmail.com>
2021-11-23 15:50:14 -0500

    updates

--- 19_api/app.py
+++ 19_api/app.py
@@ -1,18 +1,30 @@
-# 
+# Tami Takada and Tomas Acuna
 # SoftDev
-# K19 -- A RESTful Journey Skyward
+# K19 -- A RESTful Journey Skyward/Making API calls/We got an API key from NASA Open APIs and displayed an image from NASA using the key.
+# 2021-11-23
 
 from flask import Flask, render_template
 from urllib import request
+import json
 
 app = Flask(__name__) 
 
 @app.route("/")       
 def home():
-    response = request.urlopen("https://api.nasa.gov/planetary/apod?api_key=KG9wjjCgdGoznGvFe5Su5nhnfuCSlt9O10tDLjrl")
-    print(response.headers['content'])
+    key = ""
+    with open("key_nasa.txt") as f:
+        lines = f.readlines()
+        key = lines[0]
+#        print(key)
+
+    response = request.urlopen(f"https://api.nasa.gov/planetary/apod?api_key={key}")
+    dict = json.loads(response.read())
+#    print(dict)
+
     return render_template(
-        "main.html"
+        "main.html",
+        pic = dict["url"],
+        explanation = dict["explanation"]
     )
 
 if __name__ == "__main__":
